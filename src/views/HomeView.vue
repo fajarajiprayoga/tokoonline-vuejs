f2a98927c8b8d48eff2956af24fb0e4ae3881963
fajarajiprayoga <fajaraji.prayoga@gmail.com>
2022-10-01 20:50:21 +0700

    Upload project to repository

---
+++ src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    
+    <HeaderVue />
+    <CarouselVue />
+    <HomeProd />
+    <InstaVue />
+    <LogoPartner />
+    <FooterVue />
+  </div>
+</template>
+
+<script>
+import HeaderVue from '@/components/HeaderVue.vue';
+import CarouselVue from '@/components/CarouselVue.vue';
+import HomeProd from '@/components/HomeProd.vue';
+import InstaVue from '@/components/InstaVue.vue';
+import LogoPartner from '@/components/LogoPartner.vue';
+import FooterVue from '@/components/FooterVue.vue';
+
+
+export default {
+  name: 'HomeView',
+  components: {
+    HeaderVue,
+    CarouselVue,
+    HomeProd,
+    InstaVue,
+    LogoPartner,
+    FooterVue
+  }
+}
+</script>
