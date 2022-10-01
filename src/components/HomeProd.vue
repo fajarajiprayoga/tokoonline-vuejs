cdf6083734aa6e2d548622cd95fc79eb222c66a1
fajarajiprayoga <fajaraji.prayoga@gmail.com>
2022-10-01 21:01:51 +0700

    Upload project to repository

---
+++ src/components/HomeProd.vue
@@ -0,0 +1,112 @@
+<template>
+    <!-- Women Banner Section Begin -->
+    <section class="women-banner spad">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12 mt-5">
+                    <carousel class="product-slider" :autoplay="true" :dots="false" :margin="15" :nav="false">
+                        <div class="product-item">
+                            <div class="pi-pic">
+                                <img src="img/products/women-2.jpg" alt="" />
+                                <router-link to="/product">
+                                    <ul>
+                                        <li class="w-icon active">
+                                            <a href="#"><i class="icon_bag_alt"></i></a>
+                                        </li>
+                                        <li class="quick-view"><a href="#">+ Quick View</a></li>
+                                    </ul>
+                                </router-link>
+                            </div>
+                            <div class="pi-text">
+                                <div class="catagory-name">Shoes</div>
+                                <a href="#">
+                                    <h5>Guangzhou sweater</h5>
+                                </a>
+                                <div class="product-price">
+                                    $13.00
+                                </div>
+                            </div>
+                        </div>
+                        <div class="product-item">
+                            <div class="pi-pic">
+                                <img src="img/products/women-3.jpg" alt="" />
+                                <ul>
+                                    <li class="w-icon active">
+                                        <a href="#"><i class="icon_bag_alt"></i></a>
+                                    </li>
+                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
+                                </ul>
+                            </div>
+                            <div class="pi-text">
+                                <div class="catagory-name">Towel</div>
+                                <a href="#">
+                                    <h5>Pure Pineapple</h5>
+                                </a>
+                                <div class="product-price">
+                                    $34.00
+                                </div>
+                            </div>
+                        </div>
+                        <div class="product-item">
+                            <div class="pi-pic">
+                                <img src="img/products/women-4.jpg" alt="" />
+                                <ul>
+                                    <li class="w-icon active">
+                                        <a href="#"><i class="icon_bag_alt"></i></a>
+                                    </li>
+                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
+                                    <li class="w-icon">
+                                        <a href="#"><i class="fa fa-random"></i></a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="pi-text">
+                                <div class="catagory-name">Towel</div>
+                                <a href="#">
+                                    <h5>Converse Shoes</h5>
+                                </a>
+                                <div class="product-price">
+                                    $34.00
+                                </div>
+                            </div>
+                        </div>
+                        <div class="product-item">
+                            <div class="pi-pic">
+                                <img src="img/products/women-1.jpg" alt="" />
+                                <ul>
+                                    <li class="w-icon active">
+                                        <a href="#"><i class="icon_bag_alt"></i></a>
+                                    </li>
+                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
+                                    <li class="w-icon">
+                                        <a href="#"><i class="fa fa-random"></i></a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="pi-text">
+                                <div class="catagory-name">Towel</div>
+                                <a href="#">
+                                    <h5>Converse Shoes</h5>
+                                </a>
+                                <div class="product-price">
+                                    $34.00
+                                </div>
+                            </div>
+                        </div>
+                    </carousel>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Women Banner Section End -->
+</template>
+
+<script>
+import carousel from 'vue-owl-carousel'
+export default {
+    name: "HomeProd",
+    components: {
+        carousel,
+    }
+}
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,46 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import ProductView from '../views/ProductView.vue'
+import ShoppingCart from '../views/ShoppingCart.vue'
+import SuccessView from '../views/SuccessView.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/product',
+    name: 'productview',
+    component: ProductView
+  },
+  {
+    path: '/cart',
+    name: 'ShoppingCart',
+    component: ShoppingCart
+  },
+  {
+    path: '/success',
+    name: 'SuccessView',
+    component: SuccessView
+  }
+]
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
+
+export default router
